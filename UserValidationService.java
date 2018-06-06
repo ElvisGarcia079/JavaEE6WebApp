c293eb0d101793f5f87dc6783880b38d2c13242f
Elvis Garcia <elvisgarcia93@gmail.com>
2018-06-05 22:51:57 -0400

    Changed Parameters

--- UserValidationService.java
+++ UserValidationService.java
@@ -5,7 +5,7 @@ public class UserValidationService {
 	
 
 	public boolean isValidUser(String user, String password) {
-		if(user.equals("Elvis") && password.equals("DebtFree#112020")){
+		if(user.equals("Elvis") && password.equals("password")){
 			return true;
 		}
 		else {
